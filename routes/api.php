209a1eaf48662acacc67924429444b56579d8841
JvBR <joaovictor.brodr@gmail.com>
2023-12-28 23:32:52 -0300

    get single user

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/users/{id}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 
